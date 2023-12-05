89d67c234236f79d55a4fd2be77a836fce2579b7
Arthur Saito <arthur_saito1998@hotmail.com>
2023-12-05 00:07:30 -0300

    [chore] including instructions on main.py

--- projects/06/Assembler/main.py
+++ projects/06/Assembler/main.py
@@ -1,7 +1,7 @@
 from hack_assembler import HackAssembler
 
 def main():
-    asm_input_file = 'seu_programa.asm'  
+    asm_input_file = '../pong/Pong.asm'  #Change here the file to be assembled
     assembler = HackAssembler(asm_input_file)
     assembler.run()
 
